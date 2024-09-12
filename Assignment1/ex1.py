8f24285dd235c1cf1a821426c72407324fc2bd0e
Marianadesamadsen <s201205@student.dtu.dk>
2024-09-12 11:56:12 +0200

    odd and even

--- Assignment1/ex1.py
+++ Assignment1/ex1.py
@@ -2,16 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def convergence_list(N_list,uk_func):
+def convergence_list(N_list,fourier_approx,u_func,uk_func):
 
     trunc_err = []
 
     for N in N_list:
 
-        k_lin_pos = np.arange(N/2+1,10001)
-        k_lin_neg = np.arange(-10000,-(N/2+1)+1)
+        #k_lin_pos = np.arange(N/2+1,10001)
+        #k_lin_neg = np.arange(-10000,-(N/2+1)+1)
+        #trunc_err.append(2*np.pi*sum(np.abs(uk_func(np.concatenate([k_lin_neg,k_lin_pos])))**2))
 
-        trunc_err.append(2*np.pi*sum(np.abs(uk_func(np.concatenate([k_lin_neg,k_lin_pos])))**2))
+        k_lin = np.arange(-N/2,N/2+1)
+        x_lin = np.linspace(0,2*np.pi,N)
+
+        u_approx = fourier_approx(k_lin,x_lin,uk_func(k_lin))
+
+        trunc_err.append(np.max(np.abs((u_func(x_lin)-u_approx))))
 
     return trunc_err
 
@@ -22,19 +28,18 @@ def fourier_approx(k_lin,x_lin,uk):
 
     return u_approx
 
-def discrete_fourier_coefficients(u_func,N):
+def discrete_fourier_coefficients(k_lin,u_func=lambda x: 1/(2-np.cos(x))):
 
-    k_lin = np.arange(-N/2,N/2)
+    N = len(k_lin)-1
     uk_approx   = np.zeros_like(k_lin,dtype=complex)
-    h           = 2*np.pi/(N+1)
 
-    for k_idx,k in enumerate(k_lin):
+    for k_idx,k in enumerate(k_lin): 
         s = 0
-        for j in range(N):
-            xj = j*h
-            s += u_func(xj)*np.exp((-2*np.pi*1j*j*k)/N)
+        for j in range(N+1):
+            xj = 2*np.pi*j/(N+1)
+            s += u_func(xj)*np.exp(-1j*k*xj) 
 
-        uk_approx[k_idx] = s/N
+        uk_approx[k_idx] = s/(N+1)
 
     return uk_approx
 
@@ -50,19 +55,22 @@ if __name__ == "__main__":
     #u_func = lambda x: x
     
     # Fourier coefficients
-    uk_func = lambda k: 1/np.sqrt(3)*(2-np.sqrt(3))**np.abs(k)
+    uk_func = lambda k: 1/(np.sqrt(3)*(2+np.sqrt(3))**np.abs(k))
+
+    # Discrete fourier coefficients
+    uk_approx_func = lambda k: discrete_fourier_coefficients(k,u_func=u_func)
     
     #uk_func = lambda k: np.where(k == 0, np.pi + 0j, 1j / k) # test kode eksempel fra slide
 
     # Initializations
     N = 64
-    N_list = np.arange(2,N,2) # Only even numbers
-    trunc_err = convergence_list(N_list,uk_func)
-    k_lin = np.arange(-N/2,N/2)
+    N_convergence_list = np.array([4,8,16,32,64,128])
+    trunc_err = convergence_list(N_convergence_list,fourier_approx,u_func,uk_func)
+    k_lin = np.arange(-N/2,N/2+1)
     
     # Convergence plot
     plt.figure(1)
-    plt.semilogy(N_list,trunc_err)
+    plt.semilogy(N_convergence_list,trunc_err)
     plt.xlabel("N")
     plt.ylabel(r"$||\tau||^2$")
     plt.title("Convergence plot")
@@ -70,9 +78,10 @@ if __name__ == "__main__":
     # Discrete fourier transform
 
     uk_approx = {} # Using dictionary
-    for Ni in np.array([4,8,16,32,64,128]): # Changing the size of N that is the number of waves
-        uk_approx[Ni] = discrete_fourier_coefficients(u_func,Ni) # Discrete fourier transformation
-        k_lin_temp = np.arange(-Ni/2,Ni/2) 
+    for Ni in N_convergence_list: # Changing the size of N that is the number of waves
+
+        k_lin_temp = np.arange(-Ni/2,Ni/2+1) 
+        uk_approx[Ni] = discrete_fourier_coefficients(k_lin_temp,u_func) # Discrete fourier transformation
         match = match_uk(uk_func(k_lin_temp),uk_approx[Ni])      # Matching the discrete FT with the analytical
         print(match)
 
@@ -84,17 +93,30 @@ if __name__ == "__main__":
     plt.ylabel("uk")
     plt.legend()
 
+    Ni = 50
     x_lin = np.linspace(0,2*np.pi,Ni)
-
+    k_lin_temp = np.arange(-Ni/2,Ni/2+1) 
     # Approximating function using fourier coefficients
     u_approx_analytical = fourier_approx(k_lin_temp,x_lin,uk_func(k_lin_temp))
-    u_approx_discrete = fourier_approx(k_lin_temp,x_lin,uk_approx[Ni])
+    u_approx_discrete = fourier_approx(k_lin_temp,x_lin,uk_approx_func(k_lin_temp))
 
     # Plotting the approximated function values
     plt.figure(3)
     plt.plot(x_lin,u_approx_analytical.real,label="u analytical")
     plt.plot(x_lin,u_approx_discrete.real,label="u discrete")
     plt.legend()
+
+    # Comparison of convergence using analytical and discrete
+    trunc_err_approx = convergence_list(N_convergence_list,fourier_approx,u_func,uk_approx_func)
+
+    plt.figure(4)
+    plt.semilogy(N_convergence_list,trunc_err,label="Analytical")
+    plt.semilogy(N_convergence_list,trunc_err_approx,label="Discrete")
+    plt.xlabel("N")
+    plt.ylabel(r"$||\tau||^2$")
+    plt.legend()
+    plt.title("Convergence plot")
+
     plt.show()
 
 
