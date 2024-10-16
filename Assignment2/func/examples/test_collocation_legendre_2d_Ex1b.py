7aaef9b3dd06d2223e2c26f955af670f162acc04
Max <mkn.net@gmail.com>
2024-10-16 12:52:05 +0200

    mistake in change of variables

--- Assignment2/func/examples/test_collocation_legendre_2d_Ex1b.py
+++ Assignment2/func/examples/test_collocation_legendre_2d_Ex1b.py
@@ -5,67 +5,57 @@ import matplotlib.pyplot as plt
 r1 = 1
 r2 = 3
 
-def g_exact(w,theta):
-    r = (r2-r1)*(w+1)/2+r1
+def u_exact(x,y):
+    r = (r2-r1)*(x+1)/2+r1
+    theta = np.pi*(y+1)
     return (r+r1**2/r)*np.cos(theta)
 
 #import sys
 #sys.path.insert(0,r"C:\Users\maria\OneDrive - Danmarks Tekniske Universitet\Kandidat\2_semester\Advanced nummerical\AdvSciComp\Assignment2\func")
 #sys.path.append(r"C:\Users\maria\OneDrive - Danmarks Tekniske Universitet\Kandidat\2_semester\Advanced nummerical\AdvSciComp\Assignment2")
-import legendre as l
-import fourier as f
+from legendre import vander, nodes
 
-Nw = 32
-Ntheta = 33
-w = l.nodes(Nw)          # r expressed in w
-theta = f.nodes(Ntheta)  # theta
+N = 24
+x = nodes(N)
+V,Vx,_ = vander(x)
+D = Vx @ inv(V)
 
-# r variable
-Vander_w,Vw,_ = l.vander(w)
-D_w     = Vw @ inv(Vander_w)    # Computing differentiation matrix
-D_theta = f.diff_matrix(Ntheta) # Computing differentiation matrix directly
-
-# Setting up grid based on the 2 variables
-X,Y = np.meshgrid(w,theta)
+X,Y = np.meshgrid(x,x)
 
 # Tensor Product
-Dx = np.kron(D_w, np.eye(Ntheta))
-Dy = np.kron(np.eye(Nw), D_theta)
+I = np.eye(N)
+Dx = np.kron(D, I)
+Dy = np.kron(I, D)
 
 # Compute the right-hand side
-b = 0*g_exact(X,Y)
+b = np.zeros([N,N])
 
 # Identify boundary indices (first and last rows/columns in 2D)
-bc_idx_x,bc_idx_y = np.where((X == w[0]) | (X == w[-1]))
+bc_idx_x,bc_idx_y = np.where((X == x[0]) | (X == x[-1]) | (Y == x[0]) | (Y == x[-1]))
+
+# Get single index
+bc_idx = bc_idx_x * N + bc_idx_y 
 
 # Set boundary condition 
-b[bc_idx_x,bc_idx_y] = g_exact(X[bc_idx_x,bc_idx_y],Y[bc_idx_x,bc_idx_y])
+b[bc_idx_x,bc_idx_y] = u_exact(X[bc_idx_x,bc_idx_y],Y[bc_idx_x,bc_idx_y])
 
 # Right hand side
 b = b.ravel()
 
-#%%
-
 # Laplacian Operator with boundary condition
 r = ((r2-r1)*(X+1)/2+r1).ravel()
 c = 2/(r2-r1)
-A = (c/r) * Dx+ c**2 * Dx@Dx + (1/r)**2*(Dy@Dy)
+A = (c/r) * Dx + c**2 * Dx@Dx + (1/np.pi*r)**2*(Dy@Dy)
 
-#%% Boundary conditions
-
-# Compute single index
-bc_idx = bc_idx_x * Nw + bc_idx_y # This should be correct now (compare with b1 = b.ravel())
-
-# Inserting BC     
 A[bc_idx] = 0
 A[bc_idx,bc_idx] = 1
 
 # Solve linear system
 U = solve(A,b)
-U = U.reshape(Ntheta,Nw)
+U = U.reshape(N,N)
 
 # Exact solution
-U_exact = g_exact(X, Y)
+U_exact = u_exact(X, Y)
 
 # Error
 error = U - U_exact
@@ -76,22 +66,22 @@ fig, axes = plt.subplots(1, 3, figsize=(14, 4))
 # Numerical solution plot
 im1 = axes[0].pcolormesh(X, Y, U, shading='auto', cmap='viridis')
 axes[0].set_title("Numerical Solution")
-axes[0].set_xlabel(r"$w$: space")
-axes[0].set_ylabel(r"$\theta$: space")
+axes[0].set_xlabel("x: space")
+axes[0].set_ylabel("y: space")
 fig.colorbar(im1, ax=axes[0])
 
 # Exact solution plot
 im2 = axes[1].pcolormesh(X, Y, U_exact, shading='auto', cmap='viridis')
 axes[1].set_title("Exact Solution")
-axes[1].set_xlabel(r"$w$: space")
-axes[1].set_ylabel(r"$\theta$: space")
+axes[1].set_xlabel("x: space")
+axes[1].set_ylabel("y: space")
 fig.colorbar(im2, ax=axes[1])
 
 # Error plot
 im3 = axes[2].pcolormesh(X, Y, error, shading='auto', cmap='viridis')
 axes[2].set_title("Error")
-axes[2].set_xlabel(r"$w$: space")
-axes[2].set_ylabel(r"$\theta$: space")
+axes[2].set_xlabel("x: space")
+axes[2].set_ylabel("y: space")
 fig.colorbar(im3, ax=axes[2])
 
 # Display the plot
